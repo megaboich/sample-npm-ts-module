8245db0f59444864a64ec1ed21e9925b7a8ba9de
Olek Boiko <mega.boich@gmail.com>
2017-05-17 15:18:23 +0200

    Version 2

--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@ export class TheExportedClass {
     public static exportedMethod1(): string {
         return "Hey there " + new Date().toDateString();
     }
+
+    public static thisIsVersion2(): string {
+        return "Version 2 here " + new Date().getTime();
+    }
 }
